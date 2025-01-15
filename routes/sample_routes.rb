0268ad9929f33d3d858ee1388bca152a83d9ddf4
Munendra <freshbite.india@gmail.com>
2025-01-15 12:51:26 +0530

    changes

--- routes/sample_routes.rb
+++ routes/sample_routes.rb
@@ -8,7 +8,7 @@ module Sinatra
           app.get '/sample' do
             content_type :json
             results = Testing.all_records
-            results = [{"name":"naga munendra", "mobile":"70751175111"}]
+            # results = [{"name":"naga munendra", "mobile":"70751175111"}]
             results.to_json
           end
         end
