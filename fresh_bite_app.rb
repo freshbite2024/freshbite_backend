c573a1c22af0b9089ab9a6cdacd19cb76912cbbe
freshbite2024 <freshbite.india@gmail.com>
2025-01-22 13:52:59 +0530

    Update fresh_bite_app.rb

--- fresh_bite_app.rb
+++ fresh_bite_app.rb
@@ -5,13 +5,6 @@ require 'dotenv/load' # Optional for .env file
 require 'sinatra/namespace'
 require 'sinatra/activerecord'
 require 'sinatra/cross_origin'
-require 'logger'  # For custom logger
-
-# Setup custom logger to log to a file
-log_file = File.open('logs/freshbite_backend.log', 'a')
-log_file.sync = true  # Ensures logs are written in real-time
-logger = Logger.new(log_file)
-logger.level = Logger::DEBUG
 
 # Database connection setup
 ActiveRecord::Base.establish_connection(
@@ -37,17 +30,11 @@ class FreshBiteApp < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   register Sinatra::CrossOrigin
 
-  # # Set the logger for Sinatra
-  # configure do
-  #   set :logger, logger
-  # end
-
   # Include all helper modules
   helpers JwtToken
 
   # Default route for the main page
   get '/' do
-    settings.logger.info "Serving index.html"
     send_file File.join(settings.public_folder, 'index.html')
   end
 
@@ -58,14 +45,13 @@ class FreshBiteApp < Sinatra::Base
     set :session_secret, 'freshbite17012025'
     enable :sessions # Enable session handling globally
 
-    # Log the configuration settings
-    settings.logger.info "Sinatra Configuration: Cross-Origin enabled with origins: #{settings.origins}"
+    # Log the configuration settings (Sinatra automatically logs to STDOUT)
+    puts "Sinatra Configuration: Cross-Origin enabled with origins: #{settings.origins}"
   end
 
   # API namespace and routes
   namespace '/api' do
     get '/' do
-      settings.logger.info "Accessed /api endpoint"
       'Hello world! Welcome to RetailerAppStg API. This is V1 Application.'
     end
 
@@ -80,8 +66,7 @@ class FreshBiteApp < Sinatra::Base
 
   # Error handling
   error 500 do
-    # settings.logger.error "Internal Server Error: #{env['sinatra.error'].message}"
-    # 'Internal Server Error, please check logs for details.'
-    puts "ERROR #{error}"
+    puts "ERROR #{env['sinatra.error']}"
+    'Internal Server Error, please check logs for details.'
   end
 end
